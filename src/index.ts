9cb42c1d4297165e8faee876612b39650edea903
Brad <brthomiu@gmail.com>
2022-07-15 08:11:50 -0700

    Added debugging

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-    res.json({ "hello": "wolloo" })
+    res.json({ "hewobmollo": "wfreemp" })
 })
 
 const port = Number(process.env.PORT || 8080);
